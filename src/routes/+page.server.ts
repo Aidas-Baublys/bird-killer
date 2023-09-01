593bb08e633caf704dffa801241be725f170b355
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2023-09-01 11:19:45 +0300

    Update user score selection from db

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 export async function load({ locals }) {
-	const { data } = await locals.supabase.from('scores').select();
+	const session = await locals.getSession();
+	const user = session?.user;
 
+	const { data } = await locals.supabase.from('scores').select('*').eq('user_id', user?.id);
 	return {
 		score: data ?? []
 	};
