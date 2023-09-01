f588585f6763304b64df69beaa686c8d91c986f1
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2023-09-01 14:58:21 +0300

    Finish basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,35 +17,38 @@
 
 	async function saveScore() {
 		const { data } = await supabase.from('scores').select('*').eq('user_id', session?.user?.id);
-		console.log('🚀 ~ saveScore ~ data:', data);
-
-		let res;
 
 		if (data?.length) {
-			res = await supabase
+			await supabase
 				.from('scores')
 				.update({ score: scoreValue })
 				.eq('user_id', session?.user?.id)
 				.select();
 		} else {
-			res = await supabase.from('scores').insert({ user_id: session?.user?.id, score: 0 }).select();
+			await supabase
+				.from('scores')
+				.insert({ user_id: session?.user?.id, score: scoreValue })
+				.select();
 		}
-
-		// If there is a error in res, tell user to try again
-		console.log('🚀 ~ saveScore ~ res:', res);
 	}
 </script>
 
 {#if session}
 	<h2>Logged in as {session.user.email}!!!!!!!!!!</h2>
-	{scoreValue}
 	<section>
 		{#if score.length}
-			<div class="score">High Score: {score[0].score}</div>
+			{#if score[0].score >= scoreValue}
+				<div class="score">High Score: {score[0].score}</div>
+			{:else}
+				<div class="score">New High Score: {scoreValue}</div>
+				<button on:click={saveScore}>Save Score</button>
+			{/if}
+		{:else if scoreValue > 0}
+			<div class="score">New High Score: {scoreValue}</div>
+			<button on:click={saveScore}>Save Score</button>
 		{:else}
 			<div class="score">No high score yet! Kill some birds!</div>
 		{/if}
-		<button on:click={saveScore}>Save Score</button>
 		<form action="/logout" method="POST">
 			<button type="submit">Logout</button>
 		</form>
@@ -53,10 +56,8 @@
 	<GameCanvas />
 {:else}
 	<div>NOT logged in!</div>
-	<div class="auth-buttons">
-		<a href="/login" class="btn btn-primary">Login</a>
-		<a href="/register" class="btn btn-secondary">Register</a>
-	</div>
+	<a href="/login" class="btn btn-primary">Login</a>
+	<a href="/register" class="btn btn-secondary">Register</a>
 {/if}
 
 <style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
 </script>
 
 <main>
@@ -11,4 +12,8 @@
 		<input type="password" name="password" />
 		<button>Login</button>
 	</form>
+	<a href="/register" class="btn btn-secondary">Register</a>
+	{#if $page.status === 400}
+		<p>{$page.form.message}</p>
+	{/if}
 </main>
